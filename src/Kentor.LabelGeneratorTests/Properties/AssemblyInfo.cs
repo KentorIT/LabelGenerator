6242354411aa6627c1e2e62945608fc7b3b2e2cf
hsahlberg <hannah.sahlberg@gmail.com>
2017-06-05 11:27:59 +0200

    Solution renamed causing new nuget package
    Package configuration including version number and more is now placed in project.json

--- src/Kentor.LabelGeneratorTests/Properties/AssemblyInfo.cs
+++ src/Kentor.LabelGeneratorTests/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LabelGeneratorTests")]
+[assembly: AssemblyProduct("Kentor.LabelGeneratorTests")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
